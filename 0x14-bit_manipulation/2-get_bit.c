23a2d1177595c0bed513617ffa0195d02a53cb5a
khadijamohamed99 <khadijamohamed99@gmail.com>
2023-05-05 17:22:40 -0700

    2. 10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at the index
+ * @n: the number to index
+ * @index: the bit to get
+ *
+ * Return: the bit state or -1 on error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_m;
+
+	if (index > 63)
+		return (-1);
+
+	bit_m = (n >> index) & 1;
+
+	return (bit_m);
+}
